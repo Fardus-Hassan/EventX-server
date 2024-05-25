5390947e303869aa6aa279d03e7a8520f43606ca
Fardus-Hassan <webdev.fardus@gmail.com>
2024-05-26 01:01:56 +0600

    added booked collection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -22,11 +22,103 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        const EventCollection = client.db('eventDB').collection('EventCollection');
+        const BookedEventCollection = client.db('eventDB').collection('BookedEventCollection');
+
+
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
 
-        
+        app.post('/events', async (req, res) => {
+            const newEvent = req.body;
+            const result = await EventCollection.insertOne(newEvent);
+            res.send(result);
+        })
+
+        app.post('/bookedEvents', async (req, res) => {
+            const newEvent = req.body;
+            const result = await BookedEventCollection.insertOne(newEvent);
+            res.send(result);
+        })
+
+        app.get('/bookedEvents/byId/:id', async (req, res) => {
+            const id = req.params.id;
+            const cursor = await BookedEventCollection.find({ id: id });
+            const events = await cursor.toArray();
+            res.send(events);
+        })
+
+        app.get('/bookedEvents/byEmail/:email', async (req, res) => {
+            const email = req.params.email;
+            const cursor = await BookedEventCollection.find({ currentUserEmail: email });
+            const events = await cursor.toArray();
+            res.send(events)
+        })
+
+
+        app.get('/bookedEvents', async (req, res) => {
+            const cursor = await BookedEventCollection.find();
+            const events = await cursor.toArray();
+            res.send(events);
+        })
+
+        app.get('/events', async (req, res) => {
+            const cursor = await EventCollection.find();
+            const events = await cursor.toArray();
+            res.send(events);
+        })
+
+        app.get('/events/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await EventCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.get('/events/byEmail/:email', async (req, res) => {
+            const email = req.params.email;
+            const cursor = await EventCollection.find({ email: email });
+            const events = await cursor.toArray();
+            res.send(events);
+        })
+
+
+        app.get('/events/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await EventCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.put('/events/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const newEvent = req.body;
+            const query = { _id: new ObjectId(id) }
+            const event = {
+                $set: {
+                    name: newEvent.name,
+                    area: newEvent.area,
+                    Description: newEvent.Description,
+                    image: newEvent.image,
+                    price: newEvent.price,
+                }
+            }
+            const result = await EventCollection.updateOne(query, event);
+            res.send(result);
+        })
+
+        app.delete('/events/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await EventCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
 
 
 
