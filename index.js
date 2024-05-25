d8c484102e51d3ea357720e7bdb314f89d22ad4a
Fardus-Hassan <webdev.fardus@gmail.com>
2024-05-26 01:04:06 +0600

    get events data by id

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await EventCollection.deleteOne(query);
-            res.send(result)
+            res.send(result);
         })
 
 
